8e3642b70910ece43cb83f63fde61244bad10959
Valeria Cavalcanti <valeria.cavalcanti@ifpb.edu.br>
2024-02-27 14:59:37 -0300

    Aula 27/02/2024

---
+++ aulas/2024_02_27/exemplo.py
@@ -0,0 +1,15 @@
+nota1 = input('Digite nota 1: ')
+nota1 = int(nota1)
+
+nota2 = input('Digite nota 2: ')
+nota2 = int(nota2)
+
+nota3 = input('Digite nota 3: ')
+nota3 = int(nota3)
+
+nota4 = input('Digite nota 4: ')
+nota4 = int(nota4)
+
+media = (nota1 + nota2 + nota3 + nota4) / 4
+
+print(media)
